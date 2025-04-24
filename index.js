9f0aa0f94eea66a0928ee5705c1876897e412e78
Desperatiz <202360508+Desperatiz@users.noreply.github.com>
2025-04-23 22:41:04 -0600

    Implemented logic to play a game of five rounds

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-let humanScore = 0
-let computerScore = 0
-
 function getComputerChoice() {
     random = Math.floor(Math.random() * 3) + 1
     choice = ""
@@ -21,48 +18,63 @@ function getHumanChoice() {
     return choice
 }
 
-function playRound(humanChoice, computerChoice) {
-    humanChoiceCaseInsensitive = humanChoice.toLowerCase()
 
-    switch (true) {
-        case humanChoiceCaseInsensitive === "rock" && computerChoice === "rock":
-            console.log(`It's a draw! both chose ${humanChoiceCaseInsensitive}`)
-            break;
-        case humanChoiceCaseInsensitive === "rock" && computerChoice === "paper":
-            console.log(`You lose! ${computerChoice} beats ${humanChoiceCaseInsensitive}`)
-            computerScore++
-            break;
-        case humanChoiceCaseInsensitive === "rock" && computerChoice === "scissors":
-            console.log(`You win! ${humanChoiceCaseInsensitive} beats ${computerChoice}`)
-            humanScore++
-            break;
+function playGame() {
+    let humanScore = 0
+    let computerScore = 0
 
-        case humanChoiceCaseInsensitive === "paper" && computerChoice === "paper":
-            console.log(`It's a draw! both chose ${humanChoiceCaseInsensitive}`)
-            break;
-        case humanChoiceCaseInsensitive === "paper" && computerChoice === "scissors":
-            console.log(`You lose! ${computerChoice} beats ${humanChoiceCaseInsensitive}`)
-            computerScore++
-            break;
-        case humanChoiceCaseInsensitive === "paper" && computerChoice === "rock":
-            console.log(`You win! ${humanChoiceCaseInsensitive} beats ${computerChoice}`)
-            humanScore++
-            break;
-
-        case humanChoiceCaseInsensitive === "scissors" && computerChoice === "scissors":
-            console.log(`It's a draw! both chose ${humanChoiceCaseInsensitive}`)
-            break;
-        case humanChoiceCaseInsensitive === "scissors" && computerChoice === "rock":
-            console.log(`You lose! ${computerChoice} beats ${humanChoiceCaseInsensitive}`)
-            computerScore++
-            break;
-        case humanChoiceCaseInsensitive === "scissors" && computerChoice === "paper":
-            console.log(`You win! ${humanChoiceCaseInsensitive} beats ${computerChoice}`)
-            humanScore++
-            break;
-        default:
-            break;
+    function playRound(humanChoice, computerChoice) {
+        humanChoiceCaseInsensitive = humanChoice.toLowerCase()
+    
+        switch (true) {
+            case humanChoiceCaseInsensitive === "rock" && computerChoice === "rock":
+                console.log(`It's a draw! both chose ${humanChoiceCaseInsensitive}`)
+                break;
+            case humanChoiceCaseInsensitive === "rock" && computerChoice === "paper":
+                console.log(`You lose! ${computerChoice} beats ${humanChoiceCaseInsensitive}`)
+                computerScore++
+                break;
+            case humanChoiceCaseInsensitive === "rock" && computerChoice === "scissors":
+                console.log(`You win! ${humanChoiceCaseInsensitive} beats ${computerChoice}`)
+                humanScore++
+                break;
+    
+            case humanChoiceCaseInsensitive === "paper" && computerChoice === "paper":
+                console.log(`It's a draw! both chose ${humanChoiceCaseInsensitive}`)
+                break;
+            case humanChoiceCaseInsensitive === "paper" && computerChoice === "scissors":
+                console.log(`You lose! ${computerChoice} beats ${humanChoiceCaseInsensitive}`)
+                computerScore++
+                break;
+            case humanChoiceCaseInsensitive === "paper" && computerChoice === "rock":
+                console.log(`You win! ${humanChoiceCaseInsensitive} beats ${computerChoice}`)
+                humanScore++
+                break;
+    
+            case humanChoiceCaseInsensitive === "scissors" && computerChoice === "scissors":
+                console.log(`It's a draw! both chose ${humanChoiceCaseInsensitive}`)
+                break;
+            case humanChoiceCaseInsensitive === "scissors" && computerChoice === "rock":
+                console.log(`You lose! ${computerChoice} beats ${humanChoiceCaseInsensitive}`)
+                computerScore++
+                break;
+            case humanChoiceCaseInsensitive === "scissors" && computerChoice === "paper":
+                console.log(`You win! ${humanChoiceCaseInsensitive} beats ${computerChoice}`)
+                humanScore++
+                break;
+            default:
+                break;
+        }
     }
+
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();       
+
+        playRound(humanSelection, computerSelection)
+    }
+
+    console.log(`Human Score: ${humanScore}\nComputer Score: ${computerScore}`)
 }
 
-playRound(getHumanChoice(), getComputerChoice())
+playGame()
